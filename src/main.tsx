c6efc1ed5be9ef5186c006aae4ff88a374ef191e
wsmagalhaes <appsafestride@gmail.com>
2025-06-20 23:54:14 -0300

    chore: ignora node_modules via .gitignore

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,9 @@
-// main.tsx - Conteúdo gerado como placeholder
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
